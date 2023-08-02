978e634516bd9e131c8eb19f4009986969368197
Tuyishimire Eric <tuyishimireericc@gmail.com>
2023-08-02 14:57:47 +0300

    Created the WeatherApp files

---
+++ README.md
@@ -0,0 +1,84 @@
+<a name="readme-top"></a>
+
+<div align="center">
+  <h3><b>WeatheApp</b></h3>
+</div>
+
+<!-- TABLE OF CONTENTS -->
+
+# 📗 Table of Contents
+
+- [📗 Table of Contents](#-table-of-contents)
+- [📖 final-capstone-project-front-end ](#-final-capstone-project-front-end-)
+  - [🛠 Built With ](#-built-with-)
+    - [Tech Stack ](#tech-stack-)
+    - [Key Features ](#key-features-)
+  - [🚀 Live Demo ](#-live-demo-)
+  - [💻 Getting Started ](#-getting-started-)
+    - [Prerequisites](#prerequisites)
+    - [Setup](#setup)
+    - [Install](#install)
+    - [Usage](#usage)
+  - [👥 Authors ](#-authors-)
+  - [🤝 Contributing ](#-contributing-)
+  - [⭐️ Show your support ](#️-show-your-support-)
+  - [🙏 Acknowledgments ](#-acknowledgments-)
+  - [📝 License ](#-license-)
+
+<!-- PROJECT DESCRIPTION -->
+
+# 📖 Calculator<a name="about-project"></a>
+The Calculator is a simple web-based tool that performs basic arithmetic operations. It supports addition, subtraction, multiplication, division, and exponentiation. Users can input numerical values and select operators to perform instant calculations. The calculator features decimal functionality and error handling for division by zero. It provides a user-friendly interface with options to clear entries and toggle between positive and negative numbers. A convenient and versatile tool for quick calculations and accurate results.
+![image](https://github.com/TuyishimireEric/Calculator/assets/102757126/79f70caa-7937-4737-9646-465c5713f7a2)
+
+## 🚀 Live Demo <a name="live-demo"></a>
+
+- [Live Demo Link](https://tuyishimireeric.github.io/WeatherApp/)
+
+<!-- GETTING STARTED -->
+
+## 💻 Getting Started <a name="getting-started"></a>
+
+- Clone the repository or download the source code.
+- Open the index.html file in a web browser.
+
+## 👥 Author <a name="authors"></a>
+
+👤 **Tuyishimire Eric**
+
+- GitHub: [@TuyishimireEric](https://github.com/TuyishimireEric)
+- LinkedIn: [@TuyishimireEric](https://www.linkedin.com/in/TuyishimireEric/)
+- E-mail: <a href="mailto:tuyishimireericc@gmail.com">tuyishimireericc@gmail.com</a>
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+<!-- FUTURE FEATURES -->
+
+## 🤝 Contributing <a name="contributing"></a>
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](https://github.com/TuyishimireEric/WeatherApp/issues).
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+<!-- SUPPORT -->
+
+## ⭐️ Show your support <a name="support"></a>
+
+If you like this project please consider giving us a star.
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+<!-- ACKNOWLEDGEMENTS -->
+
+## 🙏 Acknowledgments <a name="acknowledgements"></a>
+
+I would like to thank **Andela** and **Odin Project** for the guidance 🙏🙏
+
+
+## 📝 License <a name="license"></a>
+
+This project is [MIT](./LICENSE) licensed.
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
---
+++ index.js
@@ -0,0 +1,140 @@
+const searchForm = document.querySelector(".search");
+const searchInput = document.querySelector("#search");
+const current_location = document.querySelector("#Location");
+const current_condition = document.querySelector("#condition");
+const current_time = document.querySelector("#current_time");
+const current_temperature= document.querySelector("#temperature");
+const feels = document.querySelector("#feels");
+const rain_chance = document.querySelector("#rain");
+const wind_speed = document.querySelector("#wind");
+const weekdays = document.querySelector("#forecastday");
+const timeForcast = document.querySelector("#timeForcast");
+const toggle_unit = document.querySelector(".toggle");
+const F = document.querySelector('.F');
+const C = document.querySelector('.C');
+const loader = document.querySelector("#loading");
+let loading=true;
+
+const isLoading =(state)=>{
+    state==true?loader.style.display="block" : loader.style.display="none";
+}
+
+let key = "a13cef89520443c7b64104405232707";
+let unit = "C";
+let place;
+let current_data;
+
+const newLocation = (location)=>{
+    localStorage.setItem('location', location);
+}
+
+const start = ()=>{
+    place = localStorage.getItem('location') || "kigali";
+    unit = localStorage.getItem('unit')|| 'C';
+
+    unit=='C'? C.classList.add("active"):F.classList.add("active");
+    getData(place);
+
+}
+
+const display=(data)=>{
+    current_location.innerHTML = `${data.Location}`;
+    current_condition.innerHTML = `${data.condition}`;
+    feels.innerHTML = `${data.feelslike} <span><sup>o</sup>${unit}</span>`;
+    rain_chance.innerHTML = `${data.chanceofrain}%`;
+    wind_speed.innerHTML = `${data.wind}kph`;
+    current_temperature.innerHTML = `${data.temperature} <span><sup>o</sup>${unit}</span>`
+    let weekdaysData = ""; 
+    const days  = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']; 
+    data.forecastday.forEach(item=>{
+        const dayNum = new Date(item.date).getDay();
+        weekdaysData += `
+            <li>
+                <h2>${days[dayNum]}</h2>
+                <p>${item.temp}<span><sup>o</sup>${unit}</span></P>
+                <img src="https:${item.image}">
+            </li>
+        `
+    });
+    weekdays.innerHTML = weekdaysData;
+    const now = new Date(data.localtime);
+    const options = { month: 'long' };
+    const currentMonth = now.toLocaleString('en-US', options);
+    current_time.innerHTML = `${now.getDate()}-${currentMonth}-${now.getFullYear()}`
+    let timeforcast = "";
+
+    for(item in data.forecasthours){
+            
+        if(item>=now.getHours()){
+            timeforcast+=`
+                <li>${item}<hr><span class="temp">${data.forecasthours[item]}<sup>o</sup>C</span>
+             `;
+        }
+    };
+}
+
+const findData=(data)=>{
+    const foundData = {
+      Location: data.location.region+", "+data.location.country,
+      feelslike: unit == "C"? data.current.feelslike_c : data.current.feelslike_f,
+      humidity: data.current.humidity,
+      condition: data.current.condition.text,
+      wind: data.current.wind_kph,
+      temperature: unit == "C"? data.current.temp_c : data.current.temp_f,
+      forecastday: data.forecast.forecastday.map(day=>{
+        const date= day.date;
+        const temp= unit == "C"? day.day.avgtemp_c: day.day.avgtemp_f;
+        const image= day.day.condition.icon;
+        return {date, temp, image};    
+    }),
+      forecasthours: data.forecast.forecastday[0].hour.map(item=>item.temp_c),
+      chanceofrain: data.forecast.forecastday[0].day.daily_chance_of_rain,
+      localtime: data.location.localtime
+    }
+
+    display(foundData);
+}
+
+async function getData(Location){
+    newLocation(Location);
+    isLoading(true);
+    const ForecastResponse = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=${key}&q=${Location}&days=7`);
+    const ForecastData = await ForecastResponse.json();
+    isLoading(false);
+
+    current_data = ForecastData;
+    searchInput.value = "";
+    findData(ForecastData);
+}   
+
+searchForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    
+    const location = searchInput.value;
+    try {
+        await getData(location);
+    } catch (error) {
+        searchInput.value = "";
+        return;
+        console.error("Error fetching data:", error);
+    }
+});
+
+const handletoggle=()=>{
+    unit = unit == 'C'? 'F' : 'C';
+    if(unit=='C'){
+        C.classList.add('active');
+        F.classList.remove('active');
+    }else {
+        F.classList.add('active');
+        C.classList.remove('active');
+    }
+
+    localStorage.setItem("unit", unit);
+    findData(current_data);
+}
+
+toggle_unit.addEventListener('click', handletoggle)
+
+
+start();
